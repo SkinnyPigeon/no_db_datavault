29a297ffdde0edca6bd9db7985ea4b7e33ae7d0f
skinnypigeon <euan.blackledge@soprasteria.com>
2021-10-25 10:32:47 +0200

    new version with no db access

---
+++ api.py
@@ -0,0 +1,13 @@
+body = {
+  "serums_id": 364,
+  "rule_id": "RULE_0df8eb8b-a469-46ae-8119-fbf98fa05b92",
+  "tags": [
+    "diagnostic"
+  ],
+  "hospital_ids": [
+    "USTAN",
+    "FCRB",
+    "ZMC"
+  ],
+  "public_key": "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCDM+DNCybR7LdizOcK1gH2P7dD\nsajGUEIoPFp7wjhgKykYkCGVQCvl55g/zdh6UI9Cd/i2IEf5wo+Ct9oihy9SnJSp\n3sOp1KESV+ElwdK3vkaIo1AUuj+E8LTe7llyJ61JJdZaozyT0PxM8jB2vIaNEdbO\nbURHcIsIDc64L0e1ZQIDAQAB\n-----END PUBLIC KEY-----"
+}
